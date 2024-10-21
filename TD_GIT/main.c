a988672fc9ee4c896fa6f4a7610530e2c3586d69
skitMex <martin.hurvois@student.junia.com>
2024-10-21 10:45:48 +0200

    Fin de l'ex 1

--- TD_GIT/main.c
+++ TD_GIT/main.c
@@ -1,4 +1,28 @@
 #include <stdio.h>
+//Martin Hurvois cir1
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+
+void InitialiserUneGrille (int grille[9][9], int n)
+{
+	for (int ligne = 0; ligne < n; ligne++)
+	{
+		for (int colonne = 0; colonne < n; colonne++)
+		{
+			grille[ligne][colonne] = 0;
+		}
+	}
+	for (int ligne = 0; ligne < n; ligne++)
+	{
+		for (int colonne = 0; colonne < n; colonne++)
+			printf("%d", grille[ligne][colonne]);
+		printf("\n");
+	}
+}
 
 int main()
 {
@@ -14,7 +38,8 @@ int main()
 		{0, 7, 0, 0, 6, 9, 0, 0, 4}
 	};
 
-	int grille_moyenne[9][9] = {
+	int grille_moyenne[9][9] =
+	{
 		{1, 0, 0, 7, 0, 6, 0, 0, 0},
 		{0, 0, 0, 0, 0, 8, 5, 6, 0},
 		{6, 0, 8, 0, 5, 3, 4, 0, 1},
@@ -25,6 +50,8 @@ int main()
 		{0, 3, 0, 0, 0, 0, 0, 0, 0},
 		{8, 4, 0, 3, 0, 1, 0, 5, 9}
 	};
+	int grilleex1[9][9];
+	InitialiserUneGrille(grilleex1, 3);
 
 	return 0;
 }
